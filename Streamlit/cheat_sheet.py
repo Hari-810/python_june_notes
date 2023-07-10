d7d2d38e6950724209564e055119425089edf3f0
Hari-810 <harijee098@gmail.com>
2023-07-10 11:07:41 +0530

    oops updated

--- Streamlit/cheat_sheet.py
+++ Streamlit/cheat_sheet.py
@@ -3,20 +3,6 @@ Streamlit Cheat Sheet
 
 App to summarise streamlit docs v1.8.0
 
-There is also an accompanying png and pdf version
-
-https://github.com/daniellewisDL/streamlit-cheat-sheet
-
-v1.8.0 October 2021
-
-Author:
-    @daniellewisDL : https://github.com/daniellewisDL
-
-Contributors:
-    @arnaudmiribel : https://github.com/arnaudmiribel
-    @akrolsmir : https://github.com/akrolsmir
-    @nathancarter : https://github.com/nathancarter
-
 """
 
 import streamlit as st
