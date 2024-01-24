6dec190ed770c6da075b9c7fe87afe146281bd3e
Hari-810 <harijee098@gmail.com>
2024-01-24 17:56:40 +0530

    updated

--- Flask/form_args_get/app.py
+++ Flask/form_args_get/app.py
@@ -2,11 +2,11 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/form', methods=['GET'])
 def get_data():
     user_input = request.args.get('data')
     if user_input:
-        return f'You entered: {user_input}'
+        return f'You entered: {user_input.upper()}'
     return render_template('index.html')
 
 if __name__ == '__main__':
