1ec56e96c0eb4f1ec8f54b0748d202e42b56c01f
Hari-810 <harijee098@gmail.com>
2024-03-19 14:20:50 +0530

    updated

--- 05-Modules and Packages/modules/one.py
+++
@@ -1,9 +0,0 @@
-def func(text):
-    print(text)
-
-print("Hi one.py")
-
-if __name__ == "__main__":
-    print("one.py is running directly")
-else:
-    print("one.py was import into another file")
