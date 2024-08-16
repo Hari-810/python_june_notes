463253fd069ae6c14657ff87953eebfe373af01f
hv <harijee098@gmail.com>
2024-08-16 10:50:47 +0530

    updated

--- 05-Modules and Packages/01_modules/two.py
+++ 05-Modules and Packages/01_modules/two.py
@@ -1,10 +1,10 @@
 import one
 
-print("top-level in two.py")
+# print("top-level in two.py")
 
 one.func()
 
-if __name__ == "__main__":
-    print("two.py is being run directly")
-else:
-    print("two.py is being imported into another module")
+# if __name__ == "__main__":
+#     print("two.py is being run directly")
+# else:
+#     print("two.py is being imported into another module")
