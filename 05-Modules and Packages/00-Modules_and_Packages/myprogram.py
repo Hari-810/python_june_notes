2ae5afdfd42f4a4c23ee349283e54ba5b35ba742
hv <harijee098@gmail.com>
2024-09-18 09:37:01 +0530

    updated

--- 05-Modules and Packages/00-Modules_and_Packages/myprogram.py
+++ 05-Modules and Packages/00-Modules_and_Packages/myprogram.py
@@ -1,17 +1,18 @@
 from MyMainPackage.some_main_script import report_main
+report_main()
+
+
 from MyMainPackage.SubPackage import mysubscript
+mysubscript.sub_report()
 
 
 from MyMainPackage.SubPackage.mysubscript import sub_report
 sub_report()
 
 
-
 from MyMainPackage.SubPackage.mysubscript import *
 sub_report()
 
 
-report_main()
 
-mysubscript.sub_report()
 
